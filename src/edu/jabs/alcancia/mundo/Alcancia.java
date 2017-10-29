3cb719f22c1b50d59000bb3495910f6b42cd7352
Joan Andrés <andres6936@live.com>
2017-10-28 20:08:08 -0500

    Modify Alcancia.java

--- src/edu/jabs/alcancia/mundo/Alcancia.java
+++ src/edu/jabs/alcancia/mundo/Alcancia.java
@@ -2,8 +2,9 @@
   Universidad Santiago de Cali (Cali - Colombia)
   Departamento de Ingeniería de Sistemas y Computación
   Licenciado bajo el esquema Academic Free License version 2.1
+
   Proyecto Cupi2 (http://cupi2.uniandes.edu.co)
-  Ejercicio: N1-Encuesta-Java
+  Ejercicio: N1-Alcancia-Java
   Autor: Joan Andrés Buriticá Salazar
   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
