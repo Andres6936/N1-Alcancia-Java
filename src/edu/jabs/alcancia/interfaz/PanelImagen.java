3fc62f5a40480a8e91abf1c220617ed483af7d12
Joan Andrés <andres6936@live.com>
2017-10-28 20:08:26 -0500

    Modify PanelImagen.java

--- src/edu/jabs/alcancia/interfaz/PanelImagen.java
+++ src/edu/jabs/alcancia/interfaz/PanelImagen.java
@@ -1,5 +1,41 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  Universidad Santiago de Cali (Cali - Colombia)
+  Departamento de Ingeniería de Sistemas y Computación
+  Licenciado bajo el esquema Academic Free License version 2.1
+
+  Proyecto Cupi2 (http://cupi2.uniandes.edu.co)
+  Ejercicio: N1-Alcancia-Java
+  Autor: Joan Andrés Buriticá Salazar
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
 package edu.jabs.alcancia.interfaz;
 
-public class PanelImagen
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * Panel con la imagen del banner.
+ */
+public class PanelImagen extends JPanel
 {
+    // -----------------------------------------------------------------
+    // Constructores
+    // -----------------------------------------------------------------
+
+    /**
+     * Constructor del panel.<br>
+     * <b>post: </b> Panel quedó inicializado.
+     */
+    public PanelImagen( )
+    {
+        JLabel imagen = new JLabel( );
+        ImageIcon icono = new ImageIcon( "data/Encabezado.png" );
+        imagen = new JLabel( "" );
+        imagen.setIcon( icono );
+        add( imagen );
+    }
+
 }
+
